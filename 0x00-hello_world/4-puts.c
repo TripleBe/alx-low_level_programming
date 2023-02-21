2bd38f239d1641a899b5c98ffa499fd7ddf5eb36
TripleBe <triplebfxacademy@gmail.com>
2023-02-20 17:58:37 -0800

    C 1 line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/*
+ * main - print a line of code using puts 
+ *
+ * return : 0
+ */
+int main(void)
+{	
+	puts("\"programming is like building a multiingual puzzle");	
+	return (0);
+}	
